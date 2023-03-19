d60712a814b5c21f82faec839b1e7b1e3093afa4
taisarantes <tais.reis.arantes@gmail.com>
2023-03-18 22:18:50 -0300

    teste um

--- teste/teste/Form1.cs
+++ teste/teste/Form1.cs
@@ -16,5 +16,10 @@ namespace teste
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button botao;
+        }
     }
 }
