d683ef91f6ecdbc99390d0d21a5dc78804cb07d3
DAVI SILVA HENRIQUE <davi.shenrique@senacsp.edu.br>
2023-05-31 21:25:00 -0300

    .ajuda

--- src/PI3/GameCore.cs
+++ src/PI3/GameCore.cs
@@ -154,6 +154,13 @@ namespace PI3{
                     partida.idJogadorAtual = int.Parse(infPartida[1]);
                     partida.rodadaAtual = int.Parse(infPartida[2]);
                     return true;
+                } else
+                {
+                    var partidaAchada = listarPartidas(PartidaState.PartidaEnum.INICIADA).Find((one) => one.id == partida.id);
+                    if(partidaAchada != null) {
+                        partida.state = PartidaState.PartidaEnum.INICIADA;
+                        update(partida);
+                    }
                 }
                 return true;
 
